1f87547b8ce03b63a090c178ad4224f63b94e108
Fernando Barboza <fernando.p.barboza@gmail.com>
2024-11-19 16:45:58 -0300

    First commit

---
+++ selenium_tests/SeleniumTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_tests;
+
+public class SeleniumTest
+{
+    [Test]
+    [Category("Selenium")]
+    public void FirstTest()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.selenium.dev/");
+        Assert.That(driver.Title, Is.EqualTo("Selenium"));
+        driver.Quit();
+    }
+}
